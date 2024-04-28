6b7d23946bea8c11399c7a5f90bcb2769ab9f481
Andy Tran <130009402+alt012ucsd@users.noreply.github.com>
2024-04-28 15:33:12 -0700

    Update part2-question16.js

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,1 +1,14 @@
+let statistics = {
+  redCars: 21,
+  blueCars: 45,
+  greenCars: 12,
+  raceCars: 5,
+  blackCars: 40,
+  rareCars: 2
+};
 
+for (let property in statistics) {
+  if (key[0] === 'r' || statiscs[key] % 2 !== 0){
+    console.log(statics[key]);
+  }
+}
